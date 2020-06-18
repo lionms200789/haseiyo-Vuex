b741891141895225eb9433c680751441ab1c577c
lionms200789 <lionms200789@gmail.com>
2020-06-18 19:58:07 +0800

    reinstall eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,36 @@
 module.exports = {
-	'env': [
-		'eslint:recommended',
-		'plugin:vue/essential',
-		'@vue/standard'
+	"env": [
+		"eslint:recommended",
+		"plugin:vue/essential",
+		"@vue/standard"
 	],
 
-	'extends': [
-		'eslint:recommended',
-		'plugin:vue/essential'
+	"extends": [
+		"eslint:recommended",
+		"plugin:vue/essential"
 	],
 
-	'globals': {
-		'Atomics': 'readonly',
-		'SharedArrayBuffer': 'readonly'
+	"globals": {
+		"Atomics": "readonly",
+		"SharedArrayBuffer": "readonly"
 	},
 
-	'parserOptions': {
-		'ecmaVersion': 2018
+	"parserOptions": {
+		"ecmaVersion": 2018
 	},
 
-	'plugins': [
-		'vue'
+	"plugins": [
+		"vue"
 	],
 
-	'rules': {
-		'quotes': [
-			'error',
-			'double'
+	"rules": {
+		"quotes": [
+			"error",
+			"double"
 		],
-		'semi': [
-			'error',
-			'always'
+		"semi": [
+			"error",
+			"always"
 		]
 	},
 
@@ -44,19 +44,19 @@ module.exports = {
 
 	parserOptions: {
 		ecmaVersion: 2018,
-		parser: 'babel-eslint'
+		parser: "babel-eslint"
 	},
 
 	rules: {
 		quotes: [
-			'error',
-			'double'
+			"error",
+			"double"
 		],
 		semi: [
-			'error',
-			'always'
+			"error",
+			"always"
 		],
-		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
 	}
 };
